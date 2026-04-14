676219a68e55c448c5207d342820506ff5e56758
Caio Gomes <caiogdo2323@gmail.com>
2026-04-13 23:48:48 -0300

    feat: register models in Django admin
    - Configured list_display, search_fields and list_filter for all models - TelemetryLog set as fully readonly to preserve log integrity - Device filter crosses FK to filter by sala via rack__sala

--- gestao/admin.py
+++ gestao/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Device, Rack, Sala, TelemetryLog
+
+
+@admin.register(Sala)
+class SalaAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'localizacao', 'criado_em']
+    search_fields = ['nome', 'localizacao']
+
+
+@admin.register(Rack)
+class RackAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'sala', 'criado_em']
+    search_fields = ['nome']
+    list_filter = ['sala']  # filtro lateral por sala
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ['serial_id', 'rack', 'status_conexao', 'bateria_pct', 'ultimo_log']
+    search_fields = ['serial_id']
+    list_filter = ['status_conexao', 'rack__sala']  # filtra por status e por sala do rack
+
+
+@admin.register(TelemetryLog)
+class TelemetryLogAdmin(admin.ModelAdmin):
+    list_display = ['device', 'status_conexao', 'bateria_pct', 'armazenamento_usado_gb', 'registrado_em']
+    search_fields = ['device__serial_id']
+    list_filter = ['status_conexao']
+    readonly_fields = ['device', 'bateria_pct', 'status_conexao', 'armazenamento_usado_gb', 'registrado_em']  # log nunca deve ser editado manualmente
